ea5b1f8d88acfbd2981fbca34c32ad2e1940f25e
Scott Eilerman <scott.eilerman@cworthy.org>
2026-08-17 12:16:06 -0600

    move stuff to ui directory, add branding

--- cstar_forge/cli.py
+++ cstar_forge/cli.py
@@ -52,7 +52,7 @@ def wizard(
 
     Extra arguments are passed through to voila.
     """
-    notebook = files("cstar_forge") / "_voila_app.ipynb"
+    notebook = files("cstar_forge.ui") / "_voila_app.ipynb"
     argv = [
         "voila",
         str(notebook),
